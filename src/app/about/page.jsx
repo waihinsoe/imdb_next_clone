1bc6c9544db96915214845dcbb2c1322ef1dccde
waihinsoe <waihinsoe332004@gmail.com>
2024-04-29 22:44:55 +0630

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus
+        vel corporis voluptatibus doloribus, dicta nemo, aspernatur suscipit
+        libero ab, placeat repellat excepturi temporibus illo perspiciatis
+        commodi blanditiis at? Non nihil perferendis facere, eum cumque ab
+        accusantium eaque iste amet quia temporibus a et nulla, dolor quos
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cupiditate
+        voluptatem dicta nulla natus pariatur? Sunt ipsam mollitia nesciunt
+        debitis accusantium porro vitae ex, voluptate earum consequatur
+        repudiandae dolore quo. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Eos cupiditate voluptatem dicta nulla natus pariatur?
+        Sunt ipsam mollitia nesciunt debitis accusantium porro vitae ex,
+        voluptate earum consequatur repudiandae dolore quo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cupiditate
+        voluptatem dicta nulla natus pariatur? Sunt ipsam mollitia nesciunt
+        debitis accusantium porro vitae ex, voluptate earum consequatur
+        repudiandae dolore quo.
+      </p>
+    </div>
+  );
+};
+
+export default About;
